027f48ac0a0050aa57e7099422b060304f3ee133
Lindsey Maddox <lindseymaddox@Lindseys-iMac.local>
2016-12-19 10:32:39 -0600

    edits to head file - latest jquery, icon generator link, removed old browse happpy and shiv  Please enter the commit message for your changes. Lines starting

--- _assets/inc/head.php
+++ _assets/inc/head.php
@@ -10,21 +10,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title></title>
 	<meta name="description" content="">
-	
-	<!--[if lt IE 8]>
-	    <p class="browsehappy">You are using an outdated browser. Please <a href="http://browsehappy.com/" target="_blank">upgrade your browser</a> to improve your experience.</p>
-	    <img class="browsehappyimg" src="/_assets/img/apple-touch-57.png"/>
-	<![endif]-->
 
-	<!-- icons -->
-	<link rel="shortcut icon" href="/_assets/img/favicon.ico">
+	<!-- generate icons here - http://realfavicongenerator.net/ -->
+	<link rel="shortcut icon" href="/_assets/img/icons/favicon.ico">
 	<link rel="apple-touch-icon" href="/_assets/img/apple-touch-57.png">
 
 	<!-- Stylesheets -->
-	<link rel="stylesheet" href="/_assets/css/styles.css" media="screen">
+	<link rel="stylesheet" href="/_assets/css/compiled.css">
 
 	<!-- JavaScript -->
-	<!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="/_assets/js/main.js"></script>
-
